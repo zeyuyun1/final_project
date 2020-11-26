0e1c1ac183d4c1f9a9723ecbe6a76c0446b48c7c
zeyuyun1 <chobitstian@berkeley.edu>
2020-11-26 03:19:57 -0800

    update color code, background changing while type

--- data_small.js
+++
@@ -1,2 +0,0 @@
-const data = {"mrc": 99608, "murakami": 182009, "murcia": 45632, "nae": 125075, "narrates": 4102};
-// console.log(data)
--- main.js
+++ main.js
@@ -1,68 +1,22 @@
-// let colorMap = new Map()
-// colorMap['wo']
-// let jsonfile = require('data.json');
-// console.log(jsonfile, 'the json obj');
-
-// console.log(123);
-// aka = ['apple']
-
-
-// async function get_data() {
-//   const response = await fetch('data_small.json');
-//   const data = await response.json();
-//   // return data[word];
-//   return data;
-// }
-
-// async function get_data_2() {
-//   const data = await get_data()
-//   console.log(data)
-//   return data;
-// }
-
-// console.log(get_data_2())
-// console.log(data['apple'])
-
-// function get_code(word) {
-//   return get_data().then(function(data) {
-//     // console.log(car)
-//     return data[word];
-
-//   });
-// }
-
-// console.log(get_code('murakami'))
-// console.log(car)
-
-// const colormap = get_color_code('murakami');
-// console.log(aka)
-// console.log(code.then())
-// let colormap;
-// fetch('data_small.json').then(response => {
-//   colormap = response.json()
-//   console.log(colormap)
-// });
-
-// const colormap = get_color_code('murakami');
-// console.log(colormap)
-// console.log(colormap)
-// console.log(colormap['mama'])
-// console.log(colormap['mama'])
-// console.log(colormap['mama'])
-
-function get_rbg_code(word) {
+function get_color(word) {
   long_code = data[word]
-  
-  r = (long_code%64)*3
-  // console.log(r)
-  long_code = Math.floor(long_code/64)
-  g = (long_code%64)*3
-  // console.log(g)
-  long_code = Math.floor(long_code/64)
-  b = (long_code%64)*3
-  // console.log(b)
-  return `(${r}, ${g}, ${b})`
+  return long_code
 }
+function changeBodyBg(color){
+  document.body.style.background = color;
+  document.getElementById("content").style.backgroundColor = color;
+  document.getElementById("myTextarea").style.backgroundColor = color;
+  document.getElementById("content").style.borderColor = color;
+  document.getElementById("myTextarea").style.borderColor = color;
+}
+function changeBodyBack(){
+  document.body.style.background = 'white';
+  document.getElementById("content").style.backgroundColor = '#f1f1f1';
+  document.getElementById("myTextarea").style.backgroundColor = '#f4e4e9';
+  document.getElementById("content").style.borderColor = 'white';
+  document.getElementById("myTextarea").style.borderColor = 'white';
+}
+
 let container = {'content' : ''}
 function handleInput(e) {
   currentText = e.target.value.toLowerCase()
@@ -71,16 +25,21 @@ function handleInput(e) {
   const text_split = currentText.split(" ")
   const block = []
   // console.log(text_split)
+  
 
   text_split.forEach((element) => {
     if (element.match("^.*[\n]")){
-      extracted = element.match("([a-z]*)[\n]")[1]
-      block.push("<span style='color:rgb"+get_rbg_code(extracted)+";'>█</span>")
+      element = element.match("([a-z]*)[\n]")[1]
+      // block.push("<span style='color:rgb"+get_rbg_code(extracted)+";'>█</span>")
+      block.push("<span style='color:"+get_color(element)+";'>█</span>")
       block.push("<br>")
+      changeBodyBack()
+      
     } else{
-      block.push("<span style='color:rgb"+get_rbg_code(element)+";'>█</span>")
+      // block.push("<span style='color:rgb"+get_rbg_code(element)+";'>█</span>")
+      block.push("<span style='color:"+get_color(element)+";'>█</span>")
     }
-
+    changeBodyBg(get_color(element))
   })
   // console.log(block)
   // console.log(block.join(''))
